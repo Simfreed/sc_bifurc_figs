add6b884d2534cf56df38370ac1a86d86b2cf73b
Simon Freedman <simfreed@gmail.com>
2021-07-27 23:31:33 -0500

    tightening up

--- pca_gene_resample.py
+++ pca_gene_resample.py
@@ -4,18 +4,22 @@ from scipy import io as scio
 from sklearn.decomposition import PCA
 import argparse
 import os
+import myfun as mf
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--dir",        type=str, help  = "output subdirectory", default='eig') 
-parser.add_argument("--neval",     type=int, help="# of evals", default=-1)
+parser.add_argument("--neval",      type=int, help="# of evals", default=-1)
 parser.add_argument("--bin_sz",     type=int, help="# of cells per bin", default=1000)
-parser.add_argument("--ov_frac",     type=float, help="fraction of bin to overlap", default=0.5)
+parser.add_argument("--ov_frac",    type=float, help="fraction of bin to overlap", default=0.5)
 parser.add_argument("--nsamp",      type=int, help="# of samps for null", default=0)
-parser.add_argument("--quest",       dest='quest', action='store_true')
 parser.add_argument("--ti",         type=int, help="first tau", default=0)
 parser.add_argument("--tf",         type=int, help="last tau", default=-1)
 parser.add_argument("--seed",       type=int, help="random number seed", default=None)
+
+parser.add_argument("--gexp_fname", type=str, help = "ncell x ngene gene expression matrix in scipy.sparse format", default='neutrophil_data/gene_expr.npz')
+parser.add_argument("--pst_fname",  type=str, help = "input/output data location",                                  default='neutrophil_data/pseudotime.txt')
+parser.add_argument("--outdir",     type=str, help = "directory for output",                                        default='neutrophil_data/eig') 
+
 
 args = parser.parse_args()
 
@@ -25,36 +29,23 @@ samp_repl = True
 bin_sz    = args.bin_sz
 overlap   = int(args.ov_frac * bin_sz)
 npc       = args.neval if args.neval > 0 else 1 
-
 np.random.seed(args.seed)
 
 # r / w
-if args.quest:
-    headdir = '/projects/p31095/simonf/out/weinreb_2020/'
-else:
-    headdir = '/Users/simonfreedman/cqub/bifurc/weinreb_2020'
-
-datdir     = 'neutrophil_data'
-gexp_fname = '{0}/gene_expr.npz'.format(datdir)
-pst_fname  = '{0}/pseudotime.txt'.format(datdir)
+print('creating output directory at {0}'.format(args.outdir))
+os.makedirs(args.outdir, exist_ok = True)
 
 print('loading gene expr matrix')
-gexp_sp    = scipy.sparse.load_npz(gexp_fname) # ~ 20s (filesize is 1.1GB)
+gexp_sp    = scipy.sparse.load_npz(args.gexp_fname) # ~ 20s (filesize is 1.1GB)
 gexp_lil   = gexp_sp.tolil() # ~4min
 print('done loading gene expr matrix')
 
-outdir    = '{0}/{1}'.format(datdir, args.dir)
-os.makedirs(outdir, exist_ok = True)
-
 # load pseudotime cell indexes
 print('binning by pseudotime')
-neut_psts         = np.genfromtxt(pst_fname, skip_header=True, dtype='int')
-
+neut_psts         = np.genfromtxt(args.pst_fname, skip_header=True, dtype='int')
 srt               = np.argsort(neut_psts[:,1])
-last_full_bin     = int(np.floor(srt.shape[0]/overlap)*overlap) - bin_sz + overlap
-neut_pst_grps     = [srt[i:(i+bin_sz)] for i in range(0,last_full_bin,overlap)]
-neut_pst_grps[-1] = np.union1d(neut_pst_grps[-1], srt[last_full_bin:])
-neut_pst_cidxs    = [np.array(neut_psts[grp,0], dtype = 'int') for grp in neut_pst_grps]
+pst_bins          = mf.get_bins(srt, bin_sz, overlap)
+neut_pst_cidxs    = [np.array(neut_psts[grp,0], dtype = 'int') for grp in pst_bins]
 npst              = len(neut_pst_cidxs)
 
 # run sampling
@@ -86,7 +77,7 @@ for t in range(ti, tf):
         pca.fit(gexp_shuf)
         cov_evals_shuf[i] = pca.explained_variance_
     
-    np.save('{0}/shuf_eval_t{1}.npy'.format(outdir, t), cov_evals_shuf)
+    np.save('{0}/shuf_eval_t{1}.npy'.format(args.outdir, t), cov_evals_shuf)
     
-np.save('{0}/dat_evec.npy'.format(outdir), cov_evecs)
-np.save('{0}/dat_eval.npy'.format(outdir), cov_evals)
+np.save('{0}/dat_evec.npy'.format(args.outdir), cov_evecs)
+np.save('{0}/dat_eval.npy'.format(args.outdir), cov_evals)
--- pcc.py
+++ pcc.py
@@ -3,14 +3,13 @@ from scipy import sparse
 import argparse
 import os
 from os import path
+import myfun as mf
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--wdir",       type=str, help="output subdirectory", default='neut_pst_corr') 
-parser.add_argument("--quest",              dest='quest', action='store_true')
 parser.add_argument("--skip_if_exists",     dest='skip_if_exists', action='store_true', help='skip pcc calculation if file already exists')
-parser.add_argument("--bin_sz",     type = int, help="number of cells per pseudotime bin", default=100) 
-parser.add_argument("--overlap",    type = float, help="fraction of cells that overlap between bins", default=0.5) 
+parser.add_argument("--bin_sz",             type = int, help="number of cells per pseudotime bin", default=100) 
+parser.add_argument("--overlap",            type = float, help="fraction of cells that overlap between bins", default=0.5) 
 
 parser.add_argument("--min_cell_gexp",     type = int, 
         help="min (non-inclusive) gene expression of an individual cell to use in pcc calc", default=0) 
@@ -19,27 +18,13 @@ parser.add_argument("--min_ncells",       type = int,
 
 parser.add_argument("--t0", type = int, help="first time point to measure correlation", default=400) 
 parser.add_argument("--tf", type = int, help="last time point to measure correlation",  default=600) 
-#parser.add_argument("--seed",       type=int, help="random number seed", default=None)
+
+parser.add_argument("--gexp_fname", type=str, help = "ncell x ngene gene expression matrix in scipy.sparse format", default='neutrophil_data/gene_expr.npz')
+parser.add_argument("--pst_fname",  type=str, help = "input/output data location",                                  default='neutrophil_data/pseudotime.txt')
+parser.add_argument("--outdir",     type=str, help = "directory for output",                                        default='neutrophil_data/corr') 
 
 args = parser.parse_args()
 
-#np.random.seed(args.seed)
-
-os.makedirs(args.wdir, exist_ok = True)
-
-if args.quest:
-    headdir = '/projects/p31095/simonf/out/weinreb_2020/'
-else:
-    headdir = '.'
-    #headdir = '/Users/simonfreedman/cqub/bifurc/weinreb_2020'
-
-#datdir     = '{0}/data'.format(headdir)
-#gexp_fname = '{0}/in_vitro_normd_counts.npz'.format(datdir)
-#pst_fname  = '{0}/stateFate_inVitro_neutrophil_pseudotime.txt'.format(datdir)
-
-datdir     = '{0}/neutrophil_data'.format(headdir)
-gexp_fname = '{0}/gene_expr.npz'.format(datdir)
-pst_fname  = '{0}/pseudotime.txt'.format(datdir)
 
 #########################################################
 ###### FUNCTION TO RUN CORRELATION WITH THRESHOLDS ######
@@ -97,47 +82,50 @@ def thresh_corr(gexp, min_cell_gexp = 0, min_ncells = 1):
 ################## BACK TO THE CODE #####################
 #########################################################
 
+os.makedirs(args.outdir, exist_ok = True)
+
 print('loading pseudotime data')
-neut_psts         = np.genfromtxt(pst_fname, skip_header=True, dtype='int')
+neut_psts         = np.genfromtxt(args.pst_fname, skip_header=True, dtype='int')
 bin_sz            = args.bin_sz
 overlap           = int(bin_sz*args.overlap)
 
 srt               = np.argsort(neut_psts[:,1])
-last_full_bin     = int(np.floor(srt.shape[0]/overlap)*overlap) - bin_sz + overlap
-neut_pst_grps     = [srt[i:(i+bin_sz)] for i in range(0,last_full_bin,overlap)]
-neut_pst_grps[-1] = np.union1d(neut_pst_grps[-1], srt[last_full_bin:])
-neut_pst_cidxs    = [np.array(neut_psts[grp,0], dtype = 'int') for grp in neut_pst_grps]
+pst_bins          = mf.get_bins(srt, bin_sz, overlap)
+neut_pst_cidxs    = [np.array(neut_psts[grp,0], dtype = 'int') for grp in pst_bins]
 ngrp              = len(neut_pst_cidxs)
 
 print('loading gene expr matrix')
-gexp_sp    = sparse.load_npz(gexp_fname) # ~ 20s (filesize is 1.1GB)
+gexp_sp    = sparse.load_npz(args.gexp_fname) # ~ 20s (filesize is 1.1GB)
 gexp_lil   = gexp_sp.tolil() # ~4min
 print('done loading gene expr matrix')
 
 ts = np.arange(args.t0,args.tf)
+
 for t in ts:
     
-    cidxs = neut_pst_cidxs[t]
+    # pull the gene expression matrix
+    cidxs  = neut_pst_cidxs[t]
     gexp_t = gexp_lil[cidxs].toarray()
 
     if args.min_cell_gexp < 0: # compute covariance, because cheap to go from there to pccs
+
         print('covariancing group {0}'.format(t))
         gidxs = np.where(gexp_lil[cidxs].sum(axis=0)>0)[1]
         gexp  = np.hstack([gexp_lil[cidxs, i].toarray() for i in gidxs])
         gcov  = np.cov(gexp.T)
 
-        np.save('{0}/cov_{1}.npy'.format(args.wdir, t), gcov)
-        np.save('{0}/nz_idxs_{1}.npy'.format(args.wdir, t), gidxs)
+        np.save('{0}/cov_{1}.npy'.format(args.outdir, t), gcov)
+        np.save('{0}/nz_idxs_{1}.npy'.format(args.outdir, t), gidxs)
+
     else:
-        pcc_f =  '{0}/pccs_{1}.npy'.format(args.wdir, t)
-        gidx_f = '{0}/gidxs_nc_{1}.npy'.format(args.wdir, t)
+        pcc_f  = '{0}/pccs_{1}.npy'.format(args.outdir, t)
+        gidx_f = '{0}/gidxs_nc_{1}.npy'.format(args.outdir, t)
         
         if args.skip_if_exists and path.exists(pcc_f) and path.exists(gidx_f):
             print('not correlating group {0} because already complete'.format(t))
+        
         else:
             print('correlating group {0}'.format(t))
             pccs, gidxs_nc = thresh_corr(gexp_t.T, args.min_cell_gexp, args.min_ncells)
             np.save(pcc_f, pccs)
             np.save(gidx_f, gidxs_nc)
-        
-    #np.save('{0}/corr_{1}.npy'.format(args.wdir, t), gcorr)
